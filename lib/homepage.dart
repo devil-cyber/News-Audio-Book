a4b590d877f812579ede749a91a0e5722f641874
Manikant Singh <mani2474695@gmail.com>
2020-11-15 10:29:05 +0530

    added Home Page for the App

---
+++ lib/homepage.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomePageState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
